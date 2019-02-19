82638821eab4b124dc948c8d48e8728322a9fae0
cuijinyue <redhat_c@163.com>
2019-02-19 19:37:15 +0800

    解决冲突

--- app.py
+++ app.py
@@ -38,22 +38,14 @@ def login():
     else:
         data={"code":1001}
         return json.dumps(data)
-<<<<<<< HEAD
+
 #退出登录
-=======
-
-
->>>>>>> a1f8523e1d2880944351631f33aa01c8ae6f0781
 @app.route('/exit',methods=['post'])
 def exit():
     ec()
     return 'ok'
 
-<<<<<<< HEAD
 #统计接口
-=======
-
->>>>>>> a1f8523e1d2880944351631f33aa01c8ae6f0781
 @app.route('/statistic',methods=['post'])
 def statistic():
     sex = statistic_friends_sex()
