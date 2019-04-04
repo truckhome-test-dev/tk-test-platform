346cc9cae046fd72ea1e172d69398b87125e0005
cuijinyue <redhat_c@163.com>
2019-04-04 14:42:58 +0800

    设置cookie有效期为7天

--- static/js/tool_p.js
+++ static/js/tool_p.js
@@ -385,8 +385,11 @@
     }
     //设置cookie
     function setCookie(name,value) {
-    document.cookie = name + '=' + escape(value);
-}
+    var Days = 7;
+    var exp = new Date();
+    exp.setTime(exp.getTime() + Days*24*60*60*1000);
+    document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+    }
     //获取cookie
     function getCookie(name)
     {
