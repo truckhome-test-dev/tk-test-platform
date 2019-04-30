f9a499f22848e49f9d027d1f987f1c5f313ad769
cuijinyue <redhat_c@163.com>
2019-04-30 16:43:32 +0800

    优化shell

--- shell.sh
+++ shell.sh
@@ -1,5 +1,8 @@
+git fetch --all && git reset --hard origin/master && git pull
 
 sed -i "s/127.0.0.1/192.168.2.92/g" `grep 127.0.0.1 -rl ./`
-
+sed -i "s/5001/5000/g" `grep 5001 -rl ./`
+sed -i "s/192.168.2.92:5001/192.168.2.92:5000/g" `grep 192.168.2.92:5001 -rl ./`
 sed -i "s/debug=True,host=/host=/g" `grep debug=True,host= -rl ./`
+
 
