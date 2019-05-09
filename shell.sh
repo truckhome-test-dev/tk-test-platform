4e80275050d304f5fe750391032f97bed8307874
olaheihei <13811081106@163.com>
2019-05-09 18:40:44 +0800

    修改脚本，增加执行权限

--- shell.sh
+++ shell.sh
@@ -6,4 +6,4 @@ sed -i "s/192.168.2.92:5001/192.168.2.92:5000/g" `grep 192.168.2.92:5001 -rl ./`
 sed -i "s/debug=True,host=/host=/g" `grep debug=True,host= -rl ./`
 sed -i "s?/home/jinyue/test?/home/tk-test-platform?g" `grep '/home/jinyue/test' -rl ./`
 
-
+chmod +x shell.sh
